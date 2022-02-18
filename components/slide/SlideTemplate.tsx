835aac0bdf956164b2f427f0bcc3f8bda1536909
Hiro101998 <asameshi0319@gmail.com>
2022-02-18 14:21:43 +0900

    スライドの完成

--- components/slide/SlideTemplate.tsx
+++ components/slide/SlideTemplate.tsx
@@ -9,13 +9,9 @@ import "swiper/css/navigation";
 // import "./styles.css";
 
 // import required modules
-import { Navigation } from "swiper";
-import { Box, Stack, Text } from "@chakra-ui/react";
-
-const testArr = [
-  { number: 1, main: "aaa" },
-  { number: 2, main: "iii" },
-];
+import { Navigation, Pagination, Scrollbar, Keyboard } from "swiper";
+import { Box, Image, Button, Text } from "@chakra-ui/react";
+import { Section2Lesson1ImgageSrc } from "../slide/imageSrc/section2/lesson1";
 
 export const SimpleSlider: FC = () => {
   return (
@@ -23,17 +19,49 @@ export const SimpleSlider: FC = () => {
       display="flex"
       justifyContent="center"
       alignItems="center"
-      h={"300"}
-      m={20}
+      m={"5%"}
+      bg={"blue.100"}
     >
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        {testArr.map((item) => {
+      <Swiper
+        navigation
+        pagination={{ clickable: true }}
+        keyboard={true}
+        modules={[Navigation, Pagination, Scrollbar]}
+        className="mySwiper"
+        scrollbar={{ draggable: true }}
+      >
+        {Section2Lesson1ImgageSrc.map((item) => {
           return (
-            <SwiperSlide key={item.number}>
-              <Text textAlign={"center"}>{item.main}</Text>
+            <SwiperSlide key={item.page}>
+              <Image
+                src={item.src}
+                alt="資料"
+                textAlign={"center"}
+                height={"50%"}
+                width={"60%"}
+                m="auto"
+              />
             </SwiperSlide>
           );
         })}
+        <SwiperSlide>
+          <Box
+            textAlign={"center"}
+            height={"50%"}
+            width={"75%"}
+            m="auto"
+            pt={200}
+          >
+            <Text fontSize={40}>
+              レクチャーはここまでです。
+              <br />
+              練習問題にチャレンジしよう！！
+            </Text>
+            <Button size="lg" mt={5} bg={"teal.300"} color={"white"}>
+              練習問題へ
+            </Button>
+          </Box>
+        </SwiperSlide>
       </Swiper>
     </Box>
   );
---
+++ components/slide/imageSrc/section2/lesson1.ts
@@ -0,0 +1,71 @@
+//呼び出すファイルからの絶対パスに合わせる必要がある。
+export const Section2Lesson1ImgageSrc = [
+  {
+    page: 1,
+    src: "../../../section2/lesson1/section2-lesson1-page1.jpg",
+  },
+  {
+    page: 2,
+    src: "../../../section2/lesson1/section2-lesson1-page2.jpg",
+  },
+  {
+    page: 3,
+    src: "../../../section2/lesson1/section2-lesson1-page3.jpg",
+  },
+  {
+    page: 4,
+    src: "../../../section2/lesson1/section2-lesson1-page4.jpg",
+  },
+  {
+    page: 5,
+    src: "../../../section2/lesson1/section2-lesson1-page5.jpg",
+  },
+  {
+    page: 6,
+    src: "../../../section2/lesson1/section2-lesson1-page6.jpg",
+  },
+  {
+    page: 7,
+    src: "../../../section2/lesson1/section2-lesson1-page7.jpg",
+  },
+  {
+    page: 8,
+    src: "../../../section2/lesson1/section2-lesson1-page8.jpg",
+  },
+  {
+    page: 9,
+    src: "../../../section2/lesson1/section2-lesson1-page9.jpg",
+  },
+  {
+    page: 10,
+    src: "../../../section2/lesson1/section2-lesson1-page10.jpg",
+  },
+  {
+    page: 11,
+    src: "../../../section2/lesson1/section2-lesson1-page11.jpg",
+  },
+  {
+    page: 12,
+    src: "../../../section2/lesson1/section2-lesson1-page12.jpg",
+  },
+  {
+    page: 13,
+    src: "../../../section2/lesson1/section2-lesson1-page13.jpg",
+  },
+  {
+    page: 14,
+    src: "../../../section2/lesson1/section2-lesson1-page14.jpg",
+  },
+  {
+    page: 15,
+    src: "../../../section2/lesson1/section2-lesson1-page15.jpg",
+  },
+  {
+    page: 16,
+    src: "../../../section2/lesson1/section2-lesson1-page16.jpg",
+  },
+  {
+    page: 17,
+    src: "../../../section2/lesson1/section2-lesson1-page17.jpg",
+  },
+];
